7336108f58468d243a4dc805c53f9a0deda3b0a4
unknown <97888az@gmail.com>
2023-01-30 23:41:13 +0330

    added ddetail of work page

--- works/urls.py
+++ works/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "work"
 
 urlpatterns = [
-    path('' , views.WorkView.as_view() , name='main_work')
+    path('' , views.WorkView.as_view() , name='main_work'),
+    path('detail/<slug:slug>' , views.WorkDetailView.as_view() , name="work_detail"),
+    path('category/<int:pk>', views.Category_details.as_view() , name='category_detail'),
 ]
--- works/views.py
+++ works/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView , ListView
+from django.shortcuts import get_object_or_404 , render
+from django.views.generic import TemplateView , ListView , View , DeleteView
 from account.models import User
 from .models import Work , Categories
 
@@ -12,3 +12,22 @@ class WorkView(ListView):
         context['user'] = User.objects.all()
         context['categories'] = Categories.objects.all()
         return context
+
+class WorkDetailView(DeleteView):
+    model = Work
+    template_name = "works/single-post.html"
+    context_object_name = 'works'
+    def get_context_data(self ,*args , **kwargs):
+        context = super().get_context_data(*args , **kwargs)
+        context['user'] = User.objects.all()
+        return context
+        
+class Category_details(View):
+    queryset = None
+    template_name = 'works/category_work_detail.html'
+    def get(self, request , pk):
+        queryset = get_object_or_404(Categories , id=pk)
+        objects = queryset.works.all()
+        categories = Categories.objects.all()
+        user = User.objects.all()
+        return render (request , self.template_name , {'id':pk , 'works':objects , 'categories':categories , 'user':user })
