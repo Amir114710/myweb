4ca30144859c10e4c5c8f4c0dc1056764d105084
unknown <97888az@gmail.com>
2023-02-12 00:06:30 +0330

    debuging2

--- account/models.py
+++ account/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
 )
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username , email , password=None):
+    def create_user(self, username , password=None):
         """
         Creates and saves a User with the given email, date of
         birth and password.
@@ -15,7 +16,6 @@ class UserManager(BaseUserManager):
 
         user = self.model(
             username=self.normalize_email(username),
-            email=self.normalize_email(email),
         )
 
         user.set_password(password)
@@ -24,12 +24,14 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, username , password=None):
         """
-        Creates and saves a superuser with the given email. and password.
+        Creates and saves a superuser with the given email, date of
+        fullname and password.
         """
         user = self.create_user(
             username,
             password=password,
         )
+
         user.is_admin = True
         user.save(using=self._db)
         return user
